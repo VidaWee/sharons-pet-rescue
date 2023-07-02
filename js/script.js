9af20c0428abde6c8ad5ee2bb364251d92ac731c
Vida <134993237+VidaWee@users.noreply.github.com>
2023-07-02 18:10:25 +1200

    Display Pet Objects

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const pets = document.querySelector(".all-pets");
 const createPet = function(name, species){
 
     const pet = {
-        name: "name",
-        species: "species",
+        name: name,
+        species: species,
         isTired: 5,
         sleep: function () {
             console.log(`${this.name} needs nap. Zzz...`);
@@ -39,7 +39,40 @@ const francine = createPet("Francine", "turtle");
 
 console.log(sora, clover, baxter, cleo, francine);
 
-clover.sleep();
-baxter.play();
+//clover.sleep();
+//baxter.play();
 
 console.log(clover, baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [  sora,
+    clover,
+    baxter,
+    cleo,
+    francine
+  ];
+
+//console.log(allPets);
+
+const showPets = function (petArray) {
+
+    pets.innerHTML = "";
+
+    for(let pet of petArray){
+        let status = "ready to play!";
+    if (pet.isTired >= 7) {
+      status = "sleeping.";
+    }
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+        pets.append(li);
+
+    }
+};
+
+statusButton.addEventListener("click",   function () {
+    showPets(allPets);
+  }
+);
